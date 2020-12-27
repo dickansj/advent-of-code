ae007a5bb2f194c286a6acbb86efac72711ff9c6
Joseph Dickan <jdickan@ucla.edu>
2020-12-27 09:59:45 +0200

    fix d02 typo

--- 2020/d02.R
+++ 2020/d02.R
@@ -3,7 +3,7 @@ library(aocodeR)
 
 ### Input
 d02 <- aoc_get_input(day = 2, year = 2020, cookie_path = "~/Projects/aoc_session_cookie.txt") %>%
-  read_table("pwd_input") %>% # Read whitespaced colums into tibble
+  read_table("pwd_input") %>% # Read whitespaced columns into tibble
   extract(pwd_input, c("low", "high", "char", "pwd"), "([0-9]+)-([0-9]+) ([a-z]): ([a-z]+)") %>% # Regex extract into columns
   mutate(across(low:high, as.integer)) # Since they're extracted as chars
 
